a93aa52e06cbbe4657258b7ea42f39c3d1953bbc
Priyanka229 <priyanka.meena@storydigital.in>
2019-05-01 21:39:01 +0530

    saveInstanceState

--- app/src/main/java/com/example/practiceapplication/asyncTask/AsyncTaskActivity.java
+++ app/src/main/java/com/example/practiceapplication/asyncTask/AsyncTaskActivity.java
@@ -10,11 +10,18 @@ import com.example.practiceapplication.R;
 public class AsyncTaskActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private static final String TEXT_STATE = "currentText";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.async_task);
+
+        mTextView = findViewById(R.id.textView1);
+
+        if(savedInstanceState!=null){
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
     }
 
     public void startTask(View view) {
@@ -23,5 +30,13 @@ public class AsyncTaskActivity extends AppCompatActivity {
 
         // Start the AsyncTask.
         new SimpleAsyncTask(mTextView).execute();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the state of the TextView
+        outState.putString(TEXT_STATE,
+                mTextView.getText().toString());
     }
 }
