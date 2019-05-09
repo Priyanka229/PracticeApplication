6f5e203064c630148be4fa0257819aeae384e772
Priyanka229 <priyanka.meena@storydigital.in>
2019-05-09 21:04:16 +0530

    asynctaskloader

---
+++ app/src/main/java/com/example/practiceapplication/AsyncTaskLoader/AsyncTaskLoader.java
@@ -0,0 +1,30 @@
+package com.example.practiceapplication.AsyncTaskLoader;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.example.practiceapplication.R;
+
+public class AsyncTaskLoader extends AppCompatActivity {
+
+    private EditText mBookInput;
+    private TextView mTitleText;
+    private TextView mAuthorText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.async_task_loader);
+
+        mBookInput = findViewById(R.id.bookInput);
+        mTitleText = findViewById(R.id.titleText);
+        mAuthorText = findViewById(R.id.authorText);
+    }
+
+    public void searchBooks(View view) {
+        // Get the search string from the input field.
+        String queryString = mBookInput.getText().toString();
+    }
+}
